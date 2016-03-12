e29b8306ceef404cb2c74e82e1ff2bf0378506e3
Tobias Büschel <tobias.buschel.15@ucl.ac.uk>
2016-03-12 03:45:10 +0100

    fixed untracked files

--- app/vendors/bower_components/br-validations/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-<a name"0.3.0"></a>
-## 0.3.0 (2015-07-15)
-
- - Initial version changelog
--- app/vendors/bower_components/ngmap/config/jsdoc/plugins/angular.js
+++
@@ -1,9 +0,0 @@
-exports.defineTags = function(dictionary) {
-  dictionary.defineTag('ngdoc', {
-    mustHaveValue: true,
-    onTagged : function(doclet, tag) {
-doclet.addTag('kind', 'class');
-      doclet.ngdoc = tag.value;
-    }
-  });
-};
